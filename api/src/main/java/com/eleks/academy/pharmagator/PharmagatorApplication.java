e3a20e5cb6ca74ec947fb5af3b81d3589a2d010a
Taras Umantsev <taras.umantsev@eleks.com>
2021-10-20 15:39:28 +0300

    Convert tabs to spaces

--- api/src/main/java/com/eleks/academy/pharmagator/PharmagatorApplication.java
+++ api/src/main/java/com/eleks/academy/pharmagator/PharmagatorApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class PharmagatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PharmagatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PharmagatorApplication.class, args);
+    }
 }
--- api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/ds/DSMedicineDto.java
+++ api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/ds/DSMedicineDto.java
@@ -10,10 +10,10 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class DSMedicineDto {
 
-	private String id;
-	private String name;
-	@JsonProperty("ціна")
-	private BigDecimal price;
-	private String manufacturer;
+    private String id;
+    private String name;
+    @JsonProperty("ціна")
+    private BigDecimal price;
+    private String manufacturer;
 
 }
